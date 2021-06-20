937a25d1424b5d393cec403cf26312826fd324d9
Khalil Ghanem <khalil_ghanem7@yahoo.com>
2021-06-20 20:14:21 +0300

    lab11 is almost done

---
+++ src/Profile.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { withAuth0 } from '@auth0/auth0-react';
+
+class Profile extends Component {
+  render() {
+    const { user,isAuthenticated } = this.props.auth0;
+    return (
+        <>
+        {isAuthenticated && 
+        <div>
+            <p>Hello {user.name}</p>
+            <p>Email : {user.email}</p>
+            <img src={user.picture} />
+           {console.log(user)}
+            
+        </div>
+        }
+        </>
+    )
+  }
+}
+
+export default withAuth0(Profile);
---
+++ src/app.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import Header from './header';
+import IsLoadingAndError from './IsLoadingAndError';
+import Footer from './footer';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Login from './login';
+import MyFavoriteBooks from './myFavoriteBooks';
+import Profile from './Profile';
+import { withAuth0 } from '@auth0/auth0-react';
+// const { user,isAuthenticated } = withAuth0;
+// const {isAuthenticated,auth0,} = withAuth0();
+
+class App extends React.Component {
+
+  render() {
+    // const { isAuthenticated } = this.props.auth0;
+    console.log('app', this.props)
+    return(
+      <>
+                      {console.log(this.props.auth0.isAuthenticated)}
+
+        <Router>
+          <IsLoadingAndError>
+            <Header />
+              <Switch>
+              <Route exact path="/">
+              {/* <Profile />
+              <Login /> */}
+                {/* TODO: if the user is logged in, render the `MyFavoriteBooks` component, if they are not, render the `Login` component */}
+                {this.props.auth0.isAuthenticated
+                  ? <MyFavoriteBooks />
+                  : <Login />
+                }
+              </Route>
+              <Route exact path="/profile">
+                 {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
+
+                {this.props.auth0.isAuthenticated
+                  ? <Profile />
+                  : null
+                }
+              </Route>
+              </Switch>
+            <Footer />
+          </IsLoadingAndError>
+        </Router>
+      </>
+    )
+  }
+}
+
+export default withAuth0(App);
